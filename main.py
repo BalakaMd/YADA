4e25736ccf4f303c9215648712e64ad4e0391d1e
BalakaMd <md.balaka@gmail.com>
2023-12-19 18:57:43 +0200

    Added functionality for writing and reading bot data from a file.

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 # import block
+import pickle
 
 
 # decorators block
+def open_file_error(func):
+    """
+        Handles exception 'FileNotFoundError' when trying to open a non-existent file.
+        :param func:
+        :return wrapper:
+        """
+
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except FileNotFoundError:
+            return {}
+
+    return inner
 
 
 # function block
+
+@open_file_error
+def load_data(path='data'):
+    """
+    Read users from the given file using 'pickle' package.
+    By default, path = 'data'.
+    :param path:
+    :return AddressBook:
+    """
+    with open(path, "rb") as file:
+        unpacked = pickle.load(file)
+    return unpacked
+
+
+def update_data(contacts: dict, path='data'):
+    """
+    Write contacts to the given file using 'pickle' package.
+    By default path = 'data'.
+    :param contacts:
+    :param path:
+    :return  None:
+    """
+    with open(path, "wb") as file:
+        pickle.dump(contacts, file)
+
+
 def parse_input(user_input: str):
     """
     Takes a string of user input and splits it into words using the split() method.
@@ -30,7 +71,7 @@ def say_hello(*args, **kwargs):
 # def_main_block
 
 def main():
-    contacts = {}
+    contacts = load_data(path='data')
     print("Welcome to the assistant bot!\n")
     while True:
         user_input = input("Enter a command: ").strip().lower()
@@ -41,9 +82,11 @@ def main():
 
         if command in ["close", "exit", "good bye"]:
             print("Good bye!")
+            update_data(contacts)
             break
         elif command in menu:
             menu[command](args, contacts)
+            update_data(contacts)
         else:
             print("Invalid command.\n")
 
