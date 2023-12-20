298cf9c680b11188f41be364e93c0484fc098241
lDirtyl <andrii.mashtaler@gmx.at>
2023-12-20 12:06:47 +0100

    color

--- address_book.py
+++ address_book.py
@@ -1,7 +1,317 @@
-# import block
+from collections import UserDict
+from datetime import date
+import re
+
+#Colors
+class Color:
+    RESET = "\033[0m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    WHITE = "\033[97m"
+    YELLOW_BOLD = "\033[93;1m"  
+    BLUE_BOLD = "\033[94;1m"    
+    MAGENTA_BOLD = "\033[95;1m" 
+    CYAN_BOLD = "\033[96;1m"    
+    WHITE_BOLD = "\033[97;1m"  
+
+def data_validator(func):
+    """
+    Wrapper function.
+    Validates a phone number and birthday date in special format.
+    :param func:
+    :return wrapper:
+    """
+
+    def inner(*args, **kwargs):
+        """
+        Handles exception 'ValueError' if the phone number or birthday is in incorrect format.
+        :param args:
+        :param kwargs:
+        :return None:
+        """
+        try:
+            func(*args, **kwargs)
+        except ValueError:
+            if func.__name__ == 'add_birthday':
+                # print("Birthday date must in this format 'DD.MM.YYYY'\n")
+                print(f"{Color.RED}Birthday date must in this format 'DD.MM.YYYY'\n{Color.RESET}")
+            else:
+                # print("Phone number must be 10 digits long\n")
+                print(f"{Color.RED}Phone number must be 10 digits long\n{Color.RESET}")
+
+    return inner
 
 
-# decorators block
+class Field:
+    """
+    Base class for record fields.
+    """
+
+    def __init__(self, value: str):
+        """
+        Initializes a Field object with a given value.
+        :param value:
+        """
+        self.value = value
+
+    def __str__(self):
+        """
+        Returns the string representation of the field value.
+        :return str(self.value):
+        """
+        return str(self.value)
 
 
-# class block
+class Name(Field):
+    """
+    Class to store contact names. Mandatory field.
+    """
+
+    def __init__(self, value: str):
+        super().__init__(value)
+        self.name = value
+
+
+class Phone(Field):
+    """
+    Class to store phone numbers. Validates the phone number using
+    validate_phone_number function for thr required format (10 digits).
+    """
+
+    def __init__(self, value: str):
+        """
+        Initializes a Phone object with a given value.
+        :param value:
+        """
+        super().__init__(value)
+        self.phone = self.validate_phone_number()
+
+    def validate_phone_number(self):
+        """
+        Validates the required format (10 digits) of the phone number.
+        :return: Raises 'ValueError' if the phone number format is invalid.
+        """
+        if re.findall(r'^\d{10}$', str(self.value.replace('+38', ''))):
+            # print("Contacts updated.\n")
+            print(f"{Color.GREEN}Contacts updated.\n{Color.RESET}")
+            return self.value
+        else:
+            # raise ValueError("Phone number must be 10 digits long.")
+            raise ValueError(f"{Color.RED}Phone number must be 10 digits long.\n{Color.RESET}")
+
+
+
+class Birthday(Field):
+    def __init__(self, value: str):
+        super().__init__(value)
+        self.birthday = self.validate_birthday()
+
+    def validate_birthday(self):
+        date_pattern = re.compile(r'^\d{2}\.\d{2}\.\d{4}$')
+        """
+        Validates the required format (DD.MM.YYYY) of the birthday date.
+        :return: Raises 'ValueError' if the birthday date format is invalid.
+        """
+        if date_pattern.match(self.value):
+            day, month, year = self.value.split('.')
+            if int(day) <= 31 and int(month) <= 12:
+                print("Birthday added.\n")
+                return date(int(year), int(month), int(day))
+            # print("Birthday date must in this format 'DD.MM.YYYY'\n")
+            print(f"{Color.RED}Birthday date must in this format 'DD.MM.YYYY'\n{Color.RESET}")
+        else:
+            # raise ValueError("Birthday date must in this format 'DD.MM.YYYY'")
+            raise ValueError(f"{Color.RED}Birthday date must in this format 'DD.MM.YYYY'{Color.RESET}")
+
+
+#CLASS Address
+class Address(Field):
+    def __init__(self, country: str, city: str, street: str, house_number: str, apartment_number: str = ''):
+        super().__init__(f"Country: {country}, City: {city}, Street: {street}, House_Number: {house_number}, Apartment_Number: {apartment_number}")
+        self.country = country
+        self.city = city
+        self.street = street
+        self.house_number = house_number
+        self.apartment_number = apartment_number
+    
+    def __str__(self):
+        return f"Country: {self.country}, City: {self.city}, Street: {self.street}, House_Number: {self.house_number}, Apartment_Number: {self.apartment_number}"
+
+class Email(Field):
+    """
+    Class to store email addresses. Validates the email using a regular expression.
+    """
+
+    def __init__(self, value: str):
+        """
+        Initializes an Email object with a given value.
+        :param value:
+        """
+        super().__init__(value)
+        self.email = self.validate_email()
+
+    def validate_email(self):
+        """
+        Validates the email address using a regular expression.
+        :return: Raises 'ValueError' if the email format is invalid.
+        """
+        email_pattern = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+        if email_pattern.match(self.value):
+            return self.value
+        else:
+            # raise ValueError("Invalid email address.")
+            raise ValueError(f"{Color.RED}Invalid email address.{Color.RESET}")
+
+    def set_email(self, value: str):
+        """
+        Set a new email address and validate it.
+        :param value:
+        :return: None
+        """
+        self.value = value
+        self.email = self.validate_email()
+        
+    def __str__(self):
+        return f"Email: {self.email}"
+
+class Record:
+    """
+    Class to store contact information including name and phone numbers.
+    """
+
+    def __init__(self, name: str):
+        """
+        Initializes a Record object with a given name.
+        :param name:
+        """
+        self.name = Name(name.lower())
+        self.phones = []
+        self.birthday = 'Unknown'
+        self.addresses = []  # Add addresses attribute
+        self.emails = []  # Add emails attribute
+
+    @data_validator
+    def add_phone(self, phone: str):
+        """
+        Adds a phone number to the contact.
+        :param phone:
+        :return None:
+        """
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone_number: str):
+        """
+        Removes a phone number from the contact.
+        :param phone_number:
+        :return None:
+        """
+        for p in self.phones:
+            if p.value == phone_number:
+                self.phones.remove(p)
+
+    @data_validator
+    def edit_phone(self, old_phone_number: str, new_phone_number: str):
+        """
+        Edits a phone number in the phone list.
+        :param old_phone_number:
+        :param new_phone_number:
+        :return None:
+        """
+        for p in self.phones:
+            if p.value == old_phone_number:
+                p.value = str(Phone(str(new_phone_number)))
+
+    def find_phone(self, phone_number: str):
+        """
+        Searches for a specific phone number in the contact.
+        :param phone_number:
+        :return phone_number if found, None otherwise:
+        """
+        for p in self.phones:
+            if p.value == phone_number:
+                return phone_number
+
+    @data_validator
+    def add_birthday(self, birthday: str):
+        self.birthday = Birthday(birthday)
+    
+    # Add addresses attribute
+    @data_validator
+    def add_address(self, country, city, street, house_number, apartment_number):
+        self.addresses.append(Address(country, city, street, house_number, apartment_number))
+        
+    @data_validator
+    def add_email(self, email: str):
+        """
+        Adds an email address to the contact.
+        :param email:
+        :return None:
+        """
+        try:
+            new_email = Email(email)
+            self.emails.append(new_email)
+            # print("Email added.")
+            print(f"{Color.GREEN}Email added.{Color.RESET}")
+        except ValueError as e:
+            # print(f"Error: {e}")
+            print(f"{Color.RED}Error{Color.RESET}: {e}")
+
+            
+    def edit_email(self, old_email: str, new_email: str):
+        """
+        Edits an email address in the email list.
+        :param old_email:
+        :param new_email:
+        :return None:
+        """
+        for e in self.emails:
+            if e.value == old_email:
+                e.set_email(new_email)
+                # print("Email updated.")
+                print(f"{Color.GREEN}Email updated.{Color.RESET}")
+                break
+        else:
+            # print("Old email not found.")
+            print(f"{Color.RED}Old email not found.{Color.RESET}")
+
+
+    def __str__(self):
+        phone_info = '; '.join([p.value for p in self.phones])
+        birthday_info = self.birthday if self.birthday != "Unknown" else "Unknown"
+        address_info = '; '.join([a.value for a in getattr(self, 'addresses', [])]) if hasattr(self, 'addresses') else "Unknown"
+
+        return f"Contact name: {self.name}, phones: {phone_info}, birthday: {birthday_info}, addresses: {address_info}"
+
+class AddressBook(UserDict):
+    """
+    Class to manage and store records in an address book.
+    """
+
+    def add_record(self, user: Record):
+        """
+        Adds a record to the address book.
+        :param user:
+        :return None:
+        """
+        self.data[user.name.value] = user
+
+    def find(self, name: str):
+        """
+        Finds a record in the address book by name.
+        :param name:
+        :return returns the string representation of the contact information:
+        """
+        return f'{self.data.get(name.lower())}\n'
+
+    def delete(self, name: str):
+        """
+        Deletes a record from the address book by name.
+        :param name:
+        :return None:
+        """
+        if name.lower() in self.data:
+            del self.data[name.lower()]
--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-# import block
 import pickle
+from address_book import AddressBook, Record, Color
+from birthday_reminder import get_birthdays_per_week
 
 
 # decorators block
+
+def input_error(func):
+    """
+    Handles exception 'ValueError', 'KeyError', 'IndexError', 'AttributeError' on user input.
+    :param func:
+    :return wrapper:
+    """
+
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            if func.__name__ == 'add_contact':
+                # print("Enter a valid command in this format --->>> <add> <name> <phone number>\n")
+                print(f"{Color.RED}Enter a valid command in this format{Color.RESET} --->>> {Color.CYAN}<add> <name> <phone number>\n{Color.RESET}")
+            elif func.__name__ == 'add_birthday':
+                # print("Enter a valid command in this format --->>> <add-birthday> <name> <DD.MM.YYYY>\n")
+                print(f"{Color.RED}Enter a valid command in this format{Color.RESET} --->>> {Color.CYAN}<add-birthday> <name> <DD.MM.YYYY>\n{Color.RESET}")
+            elif func.__name__ == 'change_contact':
+                # print("Enter a valid command in format --->>> <change> <name> <old phone number> <new phone number>\n")
+                print(f"{Color.RED}Enter a valid command in format{Color.RESET} --->>> {Color.CYAN}<change> <name> <old phone number> <new phone number>\n{Color.RESET}")
+        except (KeyError, AttributeError):
+            # print('This contact was not found in the system. Try again.\n')
+            print(f'{Color.RED}This contact was not found in the system. Try again.\n{Color.RESET}')
+        except IndexError:
+            if func.__name__ == 'show_birthday':
+                # print('Enter a command in this format --->>> <show-birthday> <name>\n')
+                print(f"{Color.RED}Enter a command in this format{Color.RESET} --->>> {Color.CYAN}<show-birthday> <name>\n{Color.RESET}")
+            else:
+                # print("Enter a command in this format --->>> <phone> <name>\n")
+                print(f"{Color.RED}Enter a command in this format{Color.RESET} --->>> {Color.CYAN}<phone> <name>\n{Color.RESET}")
+
+    return inner
+
+
 def open_file_error(func):
     """
         Handles exception 'FileNotFoundError' when trying to open a non-existent file.
@@ -14,38 +50,14 @@ def open_file_error(func):
         try:
             return func(*args, **kwargs)
         except FileNotFoundError:
-            return {}
+            return AddressBook()
 
     return inner
 
 
 # function block
 
-@open_file_error
-def load_data(path='data'):
-    """
-    Read users from the given file using 'pickle' package.
-    By default, path = 'data'.
-    :param path:
-    :return AddressBook:
-    """
-    with open(path, "rb") as file:
-        unpacked = pickle.load(file)
-    return unpacked
-
-
-def update_data(contacts: dict, path='data'):
-    """
-    Write contacts to the given file using 'pickle' package.
-    By default path = 'data'.
-    :param contacts:
-    :param path:
-    :return  None:
-    """
-    with open(path, "wb") as file:
-        pickle.dump(contacts, file)
-
-
+@input_error
 def parse_input(user_input: str):
     """
     Takes a string of user input and splits it into words using the split() method.
@@ -58,37 +70,266 @@ def parse_input(user_input: str):
     return cmd, *args
 
 
-def say_hello(*args, **kwargs):
+@input_error
+def add_contact(args: list, contacts: AddressBook):
     """
-    Prints a greeting to the command line.
+    Adding a new contact to the contact AddressBook.
+    If the contact already exists, adds another phone number.
+    :param args:
+    :param contacts:
+    :return "Contact added." if the addition was successful:
+    """
+    name, phone = args
+    if name in contacts:
+        user = contacts[name]
+        user.add_phone(phone)
+    else:
+        user = Record(name)
+        user.add_phone(phone)
+        contacts.add_record(user)
+
+
+@input_error
+def change_contact(args: list, contacts: AddressBook):
+    """
+    Stores in memory a new phone number for the username contact that already exists in the AddressBook.
+    Creates a new contact if it does not exist.
+    :param args:
+    :param contacts: 
+    :return "Contact changed." if the changing was successful: 
+    """
+    name, old_phone, new_phone = args
+    if name not in contacts:
+        print('Contact not found.\n')
+        return None
+    for n, r in contacts.data.items():
+        if n == name:
+            if r.find_phone(old_phone):
+                r.edit_phone(old_phone, new_phone)
+                break
+            else:
+                # print('Old phone number not found.\n')
+                print(f"{Color.RED}Old phone number not found.\n{Color.RESET}")
+
+
+@input_error
+def get_phone(args: list, contacts: AddressBook):
+    """
+    Returns the name and phone number if the contact is found.
+    KeyError if the contact does not exist
+    :param args:
+    :param contacts:
+    :return The name and phone number if the contact is found.
+    Return KeyError if the contact does not exist :
+    """
+    if args[0] in contacts:
+        print(f'{args[0].title()} phone(\'s) is: {[p.value for p in contacts[args[0]].phones]}\n')
+    else:
+        raise KeyError
+        
+
+@input_error
+def get_all_phones(args, contacts: AddressBook):
+    """
+    Return all saved contacts with phone numbers, birthdays, and emails to the console, if any.
+    Addresses are displayed only once in the specified format.
+    :param args:
+    :param contacts:
+    :return all saved contacts:
+    """
+    if len(contacts) == 0:
+        print("There are still no entries in your notebook. Try making one.\n")
+    else:
+        for n, r in contacts.data.items():
+            phone_info = '; '.join([p.value for p in r.phones])
+            email_info = '; '.join([e.value for e in getattr(r, 'emails', [])]) if hasattr(r, 'emails') else "Unknown"
+            birthday_info = r.birthday if r.birthday != "Unknown" else "Unknown"
+            
+            # print + Address_info
+            
+            address_info = ''
+            if hasattr(r, 'addresses') and r.addresses:
+            #     address_info = f"\nAddresses: {[a.value for a in r.addresses]}"
+
+            # print(f"Contact name: {n.title()}, phones: {phone_info}, email: {email_info}, birthday: {birthday_info}{address_info}\n")
+                address_info = print(f"{Color.CYAN_BOLD}\nAddresses{Color.RESET}: {' '.join([a.value for a in r.addresses])}")
+
+            print(f"{Color.YELLOW}Contact name{Color.RESET}: {Color.WHITE_BOLD}{n.title()}{Color.RESET}, {Color.YELLOW}phones{Color.RESET}: {phone_info}, {Color.YELLOW}email{Color.RESET}: {email_info}, {Color.YELLOW}birthday{Color.RESET}: {birthday_info}{address_info}\n")
+
+
+
+@open_file_error
+def read_data(path='data'):
+    """
+    Read users from the given file using 'pickle' package.
+    By default path = 'data'.
+    :param path:
+    :return AddressBook:
+    """
+    with open(path, "rb") as fh:
+        unpacked = pickle.load(fh)
+    return unpacked
+
+
+def write_data(contacts: AddressBook, path='data'):
+    """
+    Write contacts to the given file using 'pickle' package.
+    By default path = 'data'.
+    :param contacts:
+    :param path:
+    :return  None:
+    """
+    with open(path, "wb") as fh:
+        pickle.dump(contacts, fh)
+
+
+def hello(*args, **kwargs):
+    """
+    Prints a greeting to the console.
     :param args:
     :param kwargs:
     :return None:
     """
-    print("Hello! How can I help you?\n")
+    # print("How can I help you?\n")
+    print(f"{Color.CYAN}How can I help you?\n{Color.RESET}")
 
 
-# def_main_block
+
+def user_help(*args, **kwargs):
+    """
+    Prints a list of all commands to the console.
+    :param args:
+    :param kwargs:
+    :return None:
+    """
+    print("""
+    1. "Add" <name> <phone number> --> Adding a new contact to the contacts.
+    2. "Change" <name> <old phone number> <new phone number> --> Stores in memory a new phone number for the username.
+    3. "Phone" <name> --> Return the name and phone number of contact.
+    4. "All" --> Return all saved contacts with phone numbers, birthdays and addresses-info.
+    6. "Add-birthday" <name> <DD.MM.YYYY> --> Adding a birthday date to the contact.
+    7. "Show-birthday" <name> --> Return birthday of the requested user from contacts.
+    8. "Birthdays" --> Print a list of people who need to be greeted by days in the next week.
+    9. "add-address" <name> <country> <city> <street> <house number> <apartment number> --> Adding an address to the contact.
+    10. "add-email" <name> <email address> --> Adding an email to the contact.
+    11. "edit-email" <name> <old email address> <new email address> --> Changes the
+    12. "Close" or "Exit" --> Exit the program.
+        """)
+
+
+@input_error
+def add_birthday(args, contacts: AddressBook):
+    """
+    Adds a birthday to the user in contacts.
+    :param args:
+    :param contacts:
+    :return raise 'AttributeError' if the name does not exist in contact.:
+    """
+    name, birthday = args
+    if name in contacts:
+        user = contacts[name]
+        user.add_birthday(birthday)
+    else:
+        raise AttributeError
+
+
+@input_error
+def show_birthday(args, contacts: AddressBook):
+    """
+    Print the birthday of the requested user from contacts to the console.
+    :param args:
+    :param contacts:
+    :return Birthday of the requested user:
+    """
+    name = args[0]
+    if name in contacts:
+        # print(f'{name.title()}\'s birthday is on {contacts[name].birthday}\n')
+        print(f'{Color.YELLOW}{name.title()}{Color.RESET}\'s birthday is on {Color.WHITE_BOLD}{contacts[name].birthday}\n{Color.RESET}')
+    else:
+        raise KeyError
+
+@input_error
+def add_address(args: list, contacts: AddressBook):
+    """
+    Adds an address to the user in contacts.
+    :param args:
+    :param contacts:
+    :return "Address added." if the addition was successful:
+    """
+    name, country, city, street, house_number, apartment_number = args
+    if name in contacts:
+        user = contacts[name]
+        user.add_address(country, city, street, house_number, apartment_number)
+        # print("Address added.")
+        print(f"{Color.GREEN}Address added.{Color.RESET}")
+    else:
+        raise AttributeError
+    
+@input_error
+def add_email(args: list, contacts: AddressBook):
+    """
+    Adds an email to the user in contacts.
+    :param args:
+    :param contacts:
+    """
+    name, email = args
+    if name in contacts:
+        user = contacts[name]
+        user.add_email(email)
+        write_data(contacts)
+    else:
+        raise AttributeError
+    
+@input_error
+def edit_email(args: list, contacts: AddressBook):
+    """
+    Edits an email for the user in contacts.
+    :param args:
+    :param contacts:
+    """
+    name, old_email, new_email = args
+    if name in contacts:
+        user = contacts[name]
+        user.edit_email(old_email, new_email)
+        write_data(contacts)
+    else:
+        raise AttributeError
+
+# main block
 
 def main():
-    contacts = load_data(path='data')
-    print("Welcome to the assistant bot!\n")
+    contacts = read_data()
+    # print("Welcome to the assistant bot!\nPrint 'Help' to see all commands.\n")
+    print(f"{Color.MAGENTA_BOLD}Welcome to the assistant bot!{Color.RESET}\nPrint {Color.YELLOW_BOLD}'Help'{Color.RESET} to see all commands.\n")
     while True:
         user_input = input("Enter a command: ").strip().lower()
         command, *args = parse_input(user_input) if len(user_input) > 0 else " "
         menu = {
-            "hello": say_hello,
+            "hello": hello,
+            "add": add_contact,
+            "change": change_contact,
+            "phone": get_phone,
+            "all": get_all_phones,
+            "help": user_help,
+            'add-birthday': add_birthday,
+            'show-birthday': show_birthday,
+            'birthdays': get_birthdays_per_week,
+            "add-address": add_address,
+            "add-email": add_email,
+            "edit-email": edit_email,
         }
 
         if command in ["close", "exit", "good bye"]:
-            print("Good bye!")
-            update_data(contacts)
+            # print("Good bye!")
+            print(f"{Color.YELLOW_BOLD}Good bye!{Color.RESET}")
+            write_data(contacts)
             break
         elif command in menu:
             menu[command](args, contacts)
-            update_data(contacts)
+            write_data(contacts)
         else:
-            print("Invalid command.\n")
+            # print("Invalid command. Print 'Help' to see all commands.\n")
+            print(f"{Color.RED}Invalid command. Print 'Help' to see all commands.\n{Color.RESET}")
 
 
 if __name__ == "__main__":
