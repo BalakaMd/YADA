31b2b7d678d6b4849f04adfa6d8a95b1e7d28649
27ugeen <27ugeen@gmail.com>
2023-12-23 22:36:41 +0200

    removed failed code

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-# from setuptools import setup, find_packages
-
-# setup(
-#     name='yada',
-#     version='0.1.0',
-#     packages=find_packages(where='src'),
-    # install_requires=[
-    #     'prompt-toolkit==3.0.43',
-    #     'tabulate==0.9.0',
-    #     'wcwidth==0.2.12',
-    # ],
-    # entry_points={
-    #     'console_scripts': [
-    #         'yada = src.main:main',
-    #     ],
-    # },
-# )
-
-
 import setuptools
 
 setuptools.setup(
